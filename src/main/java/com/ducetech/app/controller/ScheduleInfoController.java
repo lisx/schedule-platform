37761b467c88b20bb5eab49121b3d888f1163b75
lisx <15110256812@qq.com>
2018-04-16 14:27:12 +0800

    统计报表

--- src/main/java/com/ducetech/app/controller/ScheduleInfoController.java
+++ src/main/java/com/ducetech/app/controller/ScheduleInfoController.java
@@ -213,7 +213,7 @@ public class ScheduleInfoController extends BaseController {
 
         HSSFRow row;
         HSSFCellStyle style = PoiUtil.getDefaultHssfCellStyle(wb);
-        Map<String, List<ScheduleInfo>> map = null;
+        Map<String, List<ScheduleInfo>> map = new HashedMap();
         Map<String, List<ScheduleInfo>> temp = null;
         if (scheduleType.equals("0")) {
             temp = groupByProperty(sis, "userId", String.class);
@@ -222,8 +222,10 @@ public class ScheduleInfoController extends BaseController {
         }
         List<ScheduleInfo> sort=scheduleInfoService.selectScheduleInfoSort(startAt, endAt, queryData, queryData, queryData, station, stationArea);
         for(ScheduleInfo info:sort){
-            List<ScheduleInfo> infoTemp=temp.get(info.getUserId());
-            map.put(info.getUserId(),infoTemp);
+            if(temp.containsKey(info.getUserId())) {
+                List<ScheduleInfo> infoTemp = temp.get(info.getUserId());
+                map.put(info.getUserId(), infoTemp);
+            }
         }
         HSSFCell cell;
         HSSFCellStyle greenStyle = wb.createCellStyle();
