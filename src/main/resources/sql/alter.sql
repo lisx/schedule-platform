51c2c682c3aa95c8f923424f5c085b20c01b7d32
lisx <15110256812@qq.com>
2018-04-23 12:18:58 +0800

    update sql

--- src/main/resources/sql/alter.sql
+++ src/main/resources/sql/alter.sql
@@ -1,3 +1,5 @@
 alter table user add column xfz_no varchar(255) DEFAULT null;
 alter table user add column zwy_no varchar(255) DEFAULT null;
 alter table user add column zwy_level varchar(255) DEFAULT null;
+alter table schedule_info_template add column serial_number varchar(20) default NULL;
+alter table schedule_info add column user_leave varchar(255) DEFAULT null;
